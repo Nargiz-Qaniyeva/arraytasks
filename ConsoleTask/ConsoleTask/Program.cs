35ab4ead7a5c6178e5c358f372f5296929a2a34f
Nargiz-Qaniyeva <tu7lyw21z@code.edu.az>
2022-03-05 18:14:50 +0400

    first commit

---
+++ ConsoleTask/ConsoleTask/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int cem = 0;
+                Console.Write("Eded 1 : ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Eded 2 : ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                for (int i = num1; i <= num2; i++)
+                {
+                    if (i % 3 == 0)
+                    {
+                        cem += i;
+                    }
+                }
+                Console.WriteLine("Ededlerin Cemi " + cem);
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
